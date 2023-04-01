a0249c7b58a27d2a6a88f6ee2a817404df90d889
JamesChan <38025067+xeden3@users.noreply.github.com>
2023-04-01 14:06:40 +0800

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace I18nAutoTranslation
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listInventoryData = new System.Windows.Forms.ListView();
+            this.No = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CN = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EN = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.JP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.KO = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PL = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Node = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_key_count = new System.Windows.Forms.Label();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_start_trans = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.prog_trans = new System.Windows.Forms.ProgressBar();
+            this.lb_prog_trans = new System.Windows.Forms.Label();
+            this.btn_stop_trans = new System.Windows.Forms.Button();
+            this.btn_open_other_lang = new System.Windows.Forms.Button();
+            this.chk_not_all = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // listInventoryData
+            // 
+            this.listInventoryData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.No,
+            this.CN,
+            this.EN,
+            this.JP,
+            this.KO,
+            this.PL,
+            this.Node});
+            this.listInventoryData.FullRowSelect = true;
+            this.listInventoryData.GridLines = true;
+            this.listInventoryData.HideSelection = false;
+            this.listInventoryData.Location = new System.Drawing.Point(12, 12);
+            this.listInventoryData.Margin = new System.Windows.Forms.Padding(4);
+            this.listInventoryData.Name = "listInventoryData";
+            this.listInventoryData.Size = new System.Drawing.Size(1010, 846);
+            this.listInventoryData.TabIndex = 0;
+            this.listInventoryData.UseCompatibleStateImageBehavior = false;
+            this.listInventoryData.View = System.Windows.Forms.View.Details;
+            // 
+            // No
+            // 
+            this.No.Text = "No";
+            this.No.Width = 36;
+            // 
+            // CN
+            // 
+            this.CN.Text = "CN(KEY)";
+            this.CN.Width = 107;
+            // 
+            // EN
+            // 
+            this.EN.Text = "EN";
+            this.EN.Width = 94;
+            // 
+            // JP
+            // 
+            this.JP.Text = "JP";
+            this.JP.Width = 81;
+            // 
+            // KO
+            // 
+            this.KO.Text = "KO";
+            this.KO.Width = 92;
+            // 
+            // PL
+            // 
+            this.PL.Text = "PL";
+            this.PL.Width = 83;
+            // 
+            // Node
+            // 
+            this.Node.Text = "Node";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 888);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "总词条数";
+            // 
+            // lb_key_count
+            // 
+            this.lb_key_count.AutoSize = true;
+            this.lb_key_count.Location = new System.Drawing.Point(152, 888);
+            this.lb_key_count.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lb_key_count.Name = "lb_key_count";
+            this.lb_key_count.Size = new System.Drawing.Size(22, 24);
+            this.lb_key_count.TabIndex = 2;
+            this.lb_key_count.Text = "0";
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(1036, 12);
+            this.btn_open.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(238, 46);
+            this.btn_open.TabIndex = 3;
+            this.btn_open.Text = "打开中文Json";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_start_trans
+            // 
+            this.btn_start_trans.Location = new System.Drawing.Point(1036, 232);
+            this.btn_start_trans.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_start_trans.Name = "btn_start_trans";
+            this.btn_start_trans.Size = new System.Drawing.Size(238, 46);
+            this.btn_start_trans.TabIndex = 4;
+            this.btn_start_trans.Text = "开始翻译";
+            this.btn_start_trans.UseVisualStyleBackColor = true;
+            this.btn_start_trans.Click += new System.EventHandler(this.btn_start_trans_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(1036, 136);
+            this.btn_save.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(238, 46);
+            this.btn_save.TabIndex = 5;
+            this.btn_save.Text = "导出翻译后文件夹";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // prog_trans
+            // 
+            this.prog_trans.Location = new System.Drawing.Point(292, 888);
+            this.prog_trans.Margin = new System.Windows.Forms.Padding(6);
+            this.prog_trans.Name = "prog_trans";
+            this.prog_trans.Size = new System.Drawing.Size(734, 24);
+            this.prog_trans.TabIndex = 6;
+            // 
+            // lb_prog_trans
+            // 
+            this.lb_prog_trans.AutoSize = true;
+            this.lb_prog_trans.BackColor = System.Drawing.Color.Transparent;
+            this.lb_prog_trans.Cursor = System.Windows.Forms.Cursors.No;
+            this.lb_prog_trans.Location = new System.Drawing.Point(1056, 888);
+            this.lb_prog_trans.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lb_prog_trans.Name = "lb_prog_trans";
+            this.lb_prog_trans.Size = new System.Drawing.Size(22, 24);
+            this.lb_prog_trans.TabIndex = 7;
+            this.lb_prog_trans.Text = "-";
+            // 
+            // btn_stop_trans
+            // 
+            this.btn_stop_trans.Enabled = false;
+            this.btn_stop_trans.Location = new System.Drawing.Point(1036, 290);
+            this.btn_stop_trans.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_stop_trans.Name = "btn_stop_trans";
+            this.btn_stop_trans.Size = new System.Drawing.Size(238, 46);
+            this.btn_stop_trans.TabIndex = 8;
+            this.btn_stop_trans.Text = "停止翻译";
+            this.btn_stop_trans.UseVisualStyleBackColor = true;
+            this.btn_stop_trans.Click += new System.EventHandler(this.btn_stop_trans_Click);
+            // 
+            // btn_open_other_lang
+            // 
+            this.btn_open_other_lang.Location = new System.Drawing.Point(1032, 78);
+            this.btn_open_other_lang.Margin = new System.Windows.Forms.Padding(6);
+            this.btn_open_other_lang.Name = "btn_open_other_lang";
+            this.btn_open_other_lang.Size = new System.Drawing.Size(238, 46);
+            this.btn_open_other_lang.TabIndex = 9;
+            this.btn_open_other_lang.Text = "导入翻译后文件夹";
+            this.btn_open_other_lang.UseVisualStyleBackColor = true;
+            this.btn_open_other_lang.Click += new System.EventHandler(this.btn_open_other_lang_Click);
+            // 
+            // chk_not_all
+            // 
+            this.chk_not_all.AutoSize = true;
+            this.chk_not_all.Location = new System.Drawing.Point(1036, 354);
+            this.chk_not_all.Name = "chk_not_all";
+            this.chk_not_all.Size = new System.Drawing.Size(258, 28);
+            this.chk_not_all.TabIndex = 10;
+            this.chk_not_all.Text = "已有翻译的不再翻译";
+            this.chk_not_all.UseVisualStyleBackColor = true;
+            this.chk_not_all.CheckedChanged += new System.EventHandler(this.chk_not_all_CheckedChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 946);
+            this.Controls.Add(this.chk_not_all);
+            this.Controls.Add(this.btn_open_other_lang);
+            this.Controls.Add(this.btn_stop_trans);
+            this.Controls.Add(this.lb_prog_trans);
+            this.Controls.Add(this.prog_trans);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_start_trans);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.lb_key_count);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listInventoryData);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "I18nAutoTranslation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listInventoryData;
+        private System.Windows.Forms.ColumnHeader No;
+        private System.Windows.Forms.ColumnHeader CN;
+        private System.Windows.Forms.ColumnHeader EN;
+        private System.Windows.Forms.ColumnHeader JP;
+        private System.Windows.Forms.ColumnHeader KO;
+        private System.Windows.Forms.ColumnHeader PL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_key_count;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_start_trans;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.ProgressBar prog_trans;
+        private System.Windows.Forms.Label lb_prog_trans;
+        private System.Windows.Forms.Button btn_stop_trans;
+        private System.Windows.Forms.Button btn_open_other_lang;
+        private System.Windows.Forms.CheckBox chk_not_all;
+        private System.Windows.Forms.ColumnHeader Node;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace I18nAutoTranslation
+{
+    public partial class frmMain : Form
+    {
+
+        private JObject _json_obj = null;
+        private bool _b_stop_trans = false;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;//该值确定是否可以选择多个文件
+            dialog.Title = "打开文件";
+            dialog.Filter = "所有文件(*.*)|*.*";
+            
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = dialog.FileName;
+                // MessageBox.Show(file);
+                _json_obj = open_json_file(file);
+
+                listInventoryData.BeginUpdate();
+                int count = _json_obj.Descendants().OfType<JValue>().Count();
+                prog_trans.Maximum = count;
+
+                foreach (JValue value in _json_obj.Descendants().OfType<JValue>())
+                {
+                    ListViewItem item = new ListViewItem();
+                    //序号
+                    item.Text = (listInventoryData.Items.Count + 1) + "";
+                    //CN
+                    item.SubItems.Add(value.ToString());
+                    //EN
+                    item.SubItems.Add("");
+                    //JP
+                    item.SubItems.Add("");
+                    //KO
+                    item.SubItems.Add("");
+                    //PL
+                    item.SubItems.Add("");
+                    //Node
+                    item.SubItems.Add(value.Path.ToString());
+
+                    listInventoryData.Items.Add(item);
+                    Console.WriteLine(value.ToString());
+                    prog_trans.Value ++;
+                }
+                prog_trans.Value = 0;
+                listInventoryData.EndUpdate();
+            }
+        }
+
+        private void save_json_folder(string file_path, JObject jobj) 
+        {
+            // 分别保存4组翻译结果
+            save_json_file_by_trans_lang(file_path, "en", 2, jobj);
+            save_json_file_by_trans_lang(file_path, "jp", 3, jobj);
+            save_json_file_by_trans_lang(file_path, "ko", 4, jobj);
+            save_json_file_by_trans_lang(file_path, "pl", 5, jobj);
+        }
+
+        private void save_json_file_by_trans_lang(string path, string lang, int subitem_idx, JObject mod_jobj) 
+        {
+            string new_path = path + "/" + lang;
+            Directory.CreateDirectory(new_path);
+            JObject new_jobj = new JObject();
+            new_jobj = mod_jobj.DeepClone() as JObject;
+            int idx = 0;
+            foreach (JValue value in new_jobj.Descendants().OfType<JValue>())
+            {
+                value.Value = listInventoryData.Items[idx].SubItems[subitem_idx].Text;
+                idx++;
+            }
+            File.WriteAllText(new_path + "/translation.json", new_jobj.ToString());
+        }
+
+        private JObject open_json_file(string file_path) 
+        {
+            try
+            {
+                using (System.IO.StreamReader file = System.IO.File.OpenText(file_path))
+                {
+                    using (JsonTextReader reader = new JsonTextReader(file))
+                    {
+                        JObject o = (JObject)JToken.ReadFrom(reader);
+                        // var value = o[key].ToString();
+                        return o;
+                    }
+                }
+            }
+            catch { }
+            return null;
+
+        }
+
+
+        private void open_json_folder(string file_path, ref JObject en_jobj, ref JObject jp_jobj, ref JObject ko_jobj, ref JObject pl_jobj)
+        {
+            // 分别保存4组翻译结果
+            en_jobj = open_json_file(file_path + "/en/translation.json");
+            jp_jobj = open_json_file(file_path + "/jp/translation.json");
+            ko_jobj = open_json_file(file_path + "/ko/translation.json");
+            pl_jobj = open_json_file(file_path + "/pl/translation.json");
+        }
+
+
+
+        private void btn_start_trans_Click(object sender, EventArgs e)
+        {
+            // Initialize the translator
+            Translator t = new Translator();
+            btn_start_trans.Enabled = false;
+            btn_stop_trans.Enabled = true;
+            // Translate the text
+            try
+            {
+                int idx = 0;
+                int count = _json_obj.Descendants().OfType<JValue>().Count();
+                prog_trans.Maximum = count;
+                lb_key_count.Text = count.ToString();
+
+                for (int i = 0; i < listInventoryData.Items.Count; i++)
+                {
+                    if (_b_stop_trans)
+                        break;
+
+                    string value = listInventoryData.Items[i].SubItems[1].Text.ToString();
+
+
+                    listInventoryData.Items[idx].SubItems[2].Text = translate_lang_by_list_idx(chk_not_all.Checked, value.ToString(), listInventoryData.Items[idx].SubItems[2].Text, t, "Chinese", "English");
+                    listInventoryData.Items[idx].SubItems[3].Text = translate_lang_by_list_idx(chk_not_all.Checked, value.ToString(), listInventoryData.Items[idx].SubItems[3].Text, t, "Chinese", "Japanese");
+                    listInventoryData.Items[idx].SubItems[4].Text = translate_lang_by_list_idx(chk_not_all.Checked, value.ToString(), listInventoryData.Items[idx].SubItems[4].Text, t, "Chinese", "Korean");
+                    listInventoryData.Items[idx].SubItems[5].Text = translate_lang_by_list_idx(chk_not_all.Checked, value.ToString(), listInventoryData.Items[idx].SubItems[5].Text, t, "Chinese", "Polish");
+
+                    lb_prog_trans.Text = string.Format("{1}/{2} {0} mSec", (int)t.TranslationTime.TotalMilliseconds, idx, lb_key_count.Text);
+
+                    listInventoryData.Update();
+
+                    idx++;
+                    prog_trans.Value += 1;
+                    Application.DoEvents();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            finally
+            {
+                
+                this.Cursor = Cursors.Default;
+            }
+
+            prog_trans.Value = 0;
+            _b_stop_trans = false;
+            btn_stop_trans.Enabled = false;
+            btn_start_trans.Enabled = true;
+        }
+
+        private string translate_lang_by_list_idx(bool not_trans_if_have_word, string cn_word, string old_word, Translator t, string from_lang, string to_lang) 
+        {
+            string w = old_word;
+            if (not_trans_if_have_word)
+            {
+                if (old_word == "")
+                {
+                    w = t.Translate(cn_word, from_lang, to_lang);
+                }
+            }
+            else 
+            {
+                w = t.Translate(cn_word, from_lang, to_lang);
+            }
+            return w;
+        }
+
+        private void btn_stop_trans_Click(object sender, EventArgs e)
+        {
+            _b_stop_trans = true;
+        }
+
+        /// <summary>
+        /// 保存Json翻译后的文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = dialog.SelectedPath;
+                save_json_folder(path, _json_obj);
+            }
+        }
+
+
+        /// <summary>
+        /// 打开Json翻译后的文件夹，并将翻译对应到List里面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_open_other_lang_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = dialog.SelectedPath;
+                JObject en_jobj = new JObject();
+                JObject jp_jobj = new JObject();
+                JObject ko_jobj = new JObject();
+                JObject pl_jobj = new JObject();
+                open_json_folder(path, ref en_jobj, ref jp_jobj, ref ko_jobj, ref pl_jobj);
+                if(en_jobj!=null)
+                    listInventoryData_update_by_jobject(2, en_jobj);
+                if (jp_jobj != null)
+                    listInventoryData_update_by_jobject(3, jp_jobj);
+                if (ko_jobj != null)
+                    listInventoryData_update_by_jobject(4, ko_jobj);
+                if (pl_jobj != null)
+                    listInventoryData_update_by_jobject(5, pl_jobj);
+
+            }
+        }
+
+        private void listInventoryData_update_by_jobject(int subitem_idx, JObject jobj) 
+        {
+            listInventoryData.BeginUpdate();
+            foreach (JValue value in jobj.Descendants().OfType<JValue>())
+            {
+                for (int i = 0; i < listInventoryData.Items.Count; i++)
+                {
+                    if (listInventoryData.Items[i].SubItems[6].Text.ToString() == value.Path.ToString())
+                    {
+                        listInventoryData.Items[i].SubItems[subitem_idx].Text = value.ToString();
+                        break;
+                    }
+
+                }
+            }
+            listInventoryData.EndUpdate();
+        }
+
+        private void chk_not_all_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
